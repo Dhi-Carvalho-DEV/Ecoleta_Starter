ac2c958f05e263b7b94b5a6bb3a97d6715513e2e
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-02-06 10:40:47 -0300

    Finalização da aplicação

--- src/server.js
+++ src/server.js
@@ -49,22 +49,31 @@ server.post('/savepoint', (req, res) => {
 
     console.log('Cadastrado com sucesso')
     console.log(this)
-    return res.send('Ok')
+    return res.render('create-point.html', { saved: true })
   }
 
   db.run(query, values, afterInsertData)
 })
 
 server.get('/search', (req, res) => {
-  db.all(`SELECT * FROM places`, function (err, rows) {
-    if (err) {
-      return console.log(err)
+  const search = req.query.search
+
+  if (search == '') {
+    return res.render('search-results.html', { total: 0 })
+  }
+
+  db.all(
+    `SELECT * FROM places WHERE city LIKE '%${search}%'`,
+    function (err, rows) {
+      if (err) {
+        return console.log(err)
+      }
+
+      const total = rows.length
+
+      return res.render('search-results.html', { places: rows, total })
     }
-
-    const total = rows.length
-
-    return res.render('search-results.html', { places: rows, total })
-  })
+  )
 })
 
 server.listen(3000)
